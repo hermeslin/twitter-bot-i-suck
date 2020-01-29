1a5c7311286438732928daff4430da424d045114
hermes <mercury.iwork@gmail.com>
2020-01-29 14:46:24 +0800

    fix(async): refine async usage

--- functions/controller/TwitterEventParser.js
+++ functions/controller/TwitterEventParser.js
@@ -1,93 +1,40 @@
-const config = require('../config/app');
-const admin = require('../utils/admin');
+const messageCreate = require('../utils/twitter/messageCreate');
 
-module.exports = (request, response) => {
+module.exports = async (request, response) => {
   // dm evnt
-  const db = admin.firestore();
-  const { userLimit, userBlacklist } = config;
   const userId = request.body.for_user_id;
   const directMessageEvents = request.body.direct_message_events;
+  const directMessageResults = [];
 
   if (directMessageEvents) {
-    directMessageEvents.forEach(async directMessageEvent => {
+    for (const directMessageEvent of directMessageEvents) {
       const {
         type,
-        created_timestamp: created_at,
         message_create: {
-          sender_id: senderId,
           message_data: messageData
         }
       } = directMessageEvent;
 
-      // block user
-      if (userBlacklist.split(',').includes(senderId)) {
-        db.collection('blacklist').doc(senderId).set({
-          created_at
-        });
-
-        db.collection('direct_message_queue').add({
-          sender: userId,
-          receiver: senderId,
-          text: 'you are not allowed to use any functions of this bot.',
-          is_send: false,
-          created_at
-        });
-        return;
-      }
+      // check user
+      directMessageResults.push(messageCreate.blacklist(userId, directMessageEvent));
 
       // subscribe
       if (type === 'message_create' && messageData.text === 'subscribe') {
-        const subscribersSnap = await db.collection('subscribers').get();
-        if (!subscribersSnap.empty && subscribersSnap.size > userLimit) {
-          db.collection('direct_message_queue').add({
-            sender: userId,
-            receiver: senderId,
-            text: 'oops, maximum number of users reached.',
-            is_send: false,
-            created_at
-          });
-        } else {
-          db.collection('subscribers').doc(senderId).set({
-            created_at
-          });
-
-          db.collection('direct_message_queue').add({
-            sender: userId,
-            receiver: senderId,
-            text: 'subscribe success.',
-            is_send: false,
-            created_at
-          })
-        }
-        return;
+        directMessageResults.push(messageCreate.subscribe(userId, directMessageEvent));
       }
 
       // unsubscribe
       if (type === 'message_create' && messageData.text === 'unsubscribe') {
-        db.collection('subscribers').doc(senderId).delete();
-
-        db.collection('direct_message_queue').add({
-          sender: userId,
-          receiver: senderId,
-          text: 'unsubscribe success.',
-          is_send: false,
-          created_at
-        });
-        return;
+        directMessageResults.push(messageCreate.unsubscribe(userId, directMessageEvent));
       }
 
-      // get user count
       if (type === 'message_create' && messageData.text === 'users') {
-        const subscribersSnap = await db.collection('subscribers').get();
-        db.collection('direct_message_queue').add({
-          sender: userId,
-          receiver: senderId,
-          text: `${subscribersSnap.size} user(s) subscribe this bot.`,
-          is_send: false,
-          created_at
-        });
-        return;
+        directMessageResults.push(messageCreate.users(userId, directMessageEvent));
       }
-    });
+    }
+
+    await Promise.all(directMessageResults);
   }
+
+  response.status(200).send('done');
 }
---
+++ functions/utils/twitter/messageCreate.js
@@ -0,0 +1,119 @@
+const config = require('../../config/app');
+const admin = require('../../utils/admin');
+
+const db = admin.firestore();
+
+module.exports.blacklist = async (userId, directMessageEvent) => {
+  const {
+    created_timestamp: created_at,
+    message_create: {
+      sender_id: messageSenderId,
+    }
+  } = directMessageEvent;
+
+  const { userBlacklist } = config;
+
+  // block user
+  try {
+    if (userBlacklist.split(',').includes(messageSenderId)) {
+      await db.collection('blacklist').doc(messageSenderId).set({
+        created_at
+      });
+
+      await db.collection('direct_message_queue').add({
+        sender: userId,
+        receiver: messageSenderId,
+        text: 'you are not allowed to use any functions of this bot.',
+        is_send: false,
+        created_at
+      });
+    }
+    return Promise.resolve('done');
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+module.exports.subscribe = async (userId, directMessageEvent) => {
+  const {
+    created_timestamp: created_at,
+    message_create: {
+      sender_id: messageSenderId,
+    }
+  } = directMessageEvent;
+
+  const { userLimit } = config;
+
+  try {
+    const subscribersSnap = await db.collection('subscribers').get();
+    const message = {
+      sender: userId,
+      receiver: messageSenderId,
+      text: 'subscribe success.',
+      is_send: false,
+      created_at
+    };
+
+    if (!subscribersSnap.empty && subscribersSnap.size > userLimit) {
+      message.text = 'oops, maximum number of users reached.';
+    } else {
+      await db.collection('subscribers').doc(messageSenderId).set({
+        created_at
+      });
+    }
+    await db.collection('direct_message_queue').add(message);
+
+    return Promise.resolve('done');
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+module.exports.unsubscribe = async (userId, directMessageEvent) => {
+  const {
+    created_timestamp: created_at,
+    message_create: {
+      sender_id: messageSenderId,
+    }
+  } = directMessageEvent;
+
+  try {
+    await db.collection('subscribers').doc(messageSenderId).delete();
+
+    await db.collection('direct_message_queue').add({
+      sender: userId,
+      receiver: messageSenderId,
+      text: 'unsubscribe success.',
+      is_send: false,
+      created_at
+    });
+
+    return Promise.resolve('done');
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+module.exports.users = async (userId, directMessageEvent) => {
+  const {
+    created_timestamp: created_at,
+    message_create: {
+      sender_id: messageSenderId,
+    }
+  } = directMessageEvent;
+
+  try {
+    const subscribersSnap = await db.collection('subscribers').get();
+
+    await db.collection('direct_message_queue').add({
+      sender: userId,
+      receiver: messageSenderId,
+      text: `${subscribersSnap.size} user(s) subscribe this bot.`,
+      is_send: false,
+      created_at
+    });
+    return Promise.resolve('done');
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
